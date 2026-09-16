527751bcff9037de46d3ac5676c3d6e35e49e795
AntoBadami <cantobadami@gmail.com>
2026-09-16 13:15:39 -0300

    primitivas de cilindro y cono

--- include/Primitives.h
+++ include/Primitives.h
@@ -9,4 +9,8 @@ namespace primitives {
 // Los índices siguen orden antihorario mirando cada cara desde afuera.
 MeshData cube(float scale_x = 1.0f, float scale_y = 1.0f, float scale_z = 1.0f);
 
+MeshData cylinder(float radio, float largo, unsigned gajos, unsigned anillos = 1U);
+
+MeshData cone(float radio, float conicidad, unsigned gajos, unsigned anillos = 1U);
+
 } // namespace primitives
